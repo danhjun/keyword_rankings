a5bc510f2816c21c4109564594be7f7aaf470f3c
danhjun <danieljun3@gmail.com>
2023-05-22 14:42:22 -0400

    Added changes to app.py to show rank up, down, same

--- keyword-rankings/src/app.py
+++ keyword-rankings/src/app.py
@@ -2,12 +2,11 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from dateutil import parser
 
 # Load CSV data using Kedro's data catalog
 @st.cache
 def load_data():
-    df = pd.read_csv(r"C:\Users\Dan\Desktop\keyword_rankings\keyword-rankings\data\01_raw\phonk.csv", index_col=0)
+    df = pd.read_csv(r"C:\Users\Dan\Desktop\keyword_rankings\keyword-rankings\data\01_raw\5-22gym hardstyle.csv", index_col=0)
     return df
 
 # Load data
@@ -21,11 +20,11 @@ selected_country = st.selectbox("Select a country", countries)
 data = data.loc[:, ~data.columns.str.startswith('Unnamed')]
 
 # Plot the data for the selected country
-fig, ax = plt.subplots(figsize=(12, 8))  # Adjust the figure size as desired
+fig, ax = plt.subplots(figsize=(26, 14))  # Adjust the figure size as desired
 
 # Select a subset of dates to display
 num_dates = data.shape[1]  # Total number of dates
-num_display_dates = 20  # Number of dates to display (increase this value for more dates)
+num_display_dates = 130  # Number of dates to display (increase this value for more dates)
 display_dates = np.linspace(0, num_dates - 1, num=num_display_dates, dtype=int)  # Select evenly spaced dates
 dates = data.columns[display_dates]
 
@@ -34,8 +33,8 @@ data_numeric = data.loc[selected_country, dates].apply(pd.to_numeric, errors='co
 
 ax.plot(dates, data_numeric, marker='o', linestyle='-', label='Data')
 ax.set_xlabel('Date')
-ax.set_ylabel('Value')
-ax.set_title(f'{selected_country} Data')
+ax.set_ylabel('Ranking')
+ax.set_title(f'{selected_country}', loc='left')
 
 # Reduce number of visible ticks on the x-axis
 num_ticks = num_display_dates
@@ -54,20 +53,91 @@ ax.set_ylim(min_value, max_value)
 increment_marks = np.arange(min_value, max_value + 1, 5)  # Adjust the increment as desired
 ax.set_yticks(increment_marks)
 
+# Set y-axis minor increment marks (increment by 1)
+increment_marks_minor = np.arange(min_value, max_value + 1, 1)  # Adjust the minor increment as desired
+ax.set_yticks(increment_marks_minor, minor=True)
+ax.yaxis.grid(True, which='minor', linestyle='--', alpha=0.5)  # Add gridlines for minor increment
+
 # Add data point markers
 ax.plot(dates, data_numeric, marker='o', linestyle='-', label='Data')
 
 # Add grid lines
 ax.grid(True, linestyle='--', alpha=0.5)
 
-# Add data point labels
-date_labels = [parser.parse(date).strftime('%b %d') for date in dates]  # Format dates as desired
-for date, value, label in zip(dates, data_numeric, date_labels):
-    ax.annotate(label, xy=(date, value), xytext=(10, 10),
-                textcoords='offset points', ha='left', va='bottom', fontsize=8)
-
 # Rotate x-axis labels for better readability
 plt.xticks(rotation=45)
 
 # Display the plot using Streamlit
 st.pyplot(fig)
+
+# Load CSV data using Kedro's data catalog
+@st.cache
+def load_data():
+    df = pd.read_csv(r"C:\Users\Dan\Desktop\keyword_rankings\keyword-rankings\data\01_raw\5-22gym hardstyle.csv", index_col=0)
+    return df
+
+# Load data
+data = load_data()
+
+# Select dates
+dates = data.columns
+start_date = st.selectbox("Select start date", dates)
+end_date = st.selectbox("Select end date", dates)
+
+# Get the ranking for the selected dates
+start_ranking = data[start_date].apply(pd.to_numeric, errors='coerce')
+end_ranking = data[end_date].apply(pd.to_numeric, errors='coerce')
+
+# Find countries with rank up, rank down, and rank stays the same
+countries_rank_up = end_ranking[end_ranking > start_ranking]
+countries_rank_down = end_ranking[end_ranking < start_ranking]
+countries_rank_same = end_ranking[end_ranking == start_ranking]
+
+# Display countries with rank up, rank down, and rank stays the same side by side
+col1, col2, col3 = st.beta_columns(3)
+
+# Display countries with rank up
+with col1:
+    st.subheader(f"Countries with Rank Up from {start_date} to {end_date}")
+    rank_up_data = {
+        'Country': [],
+        'Rank Difference': []
+    }
+    for country, rank_up in countries_rank_up.items():
+        rank_difference = rank_up - start_ranking[country]
+        rank_up_data['Country'].append(country)
+        rank_up_data['Rank Difference'].append(rank_difference)
+
+    rank_up_df = pd.DataFrame(rank_up_data)
+    rank_up_df = rank_up_df.sort_values(by='Rank Difference', ascending=False)  # Sort in descending order
+    st.dataframe(rank_up_df)
+
+# Display countries with rank down
+with col2:
+    st.subheader(f"Countries with Rank Down from {start_date} to {end_date}")
+    rank_down_data = {
+        'Country': [],
+        'Rank Difference': []
+    }
+    for country, rank_down in countries_rank_down.items():
+        rank_difference = start_ranking[country] - rank_down
+        rank_down_data['Country'].append(country)
+        rank_down_data['Rank Difference'].append(rank_difference)
+
+    rank_down_df = pd.DataFrame(rank_down_data)
+    rank_down_df = rank_down_df.sort_values(by='Rank Difference', ascending=False)  # Sort in descending order
+    st.dataframe(rank_down_df)
+
+# Display countries with rank stays the same
+with col3:
+    st.subheader(f"Countries with Rank Stays the Same from {start_date} to {end_date}")
+    rank_same_data = {
+        'Country': [],
+        'Rank': []
+    }
+    for country, rank_same in countries_rank_same.items():
+        rank_same_data['Country'].append(country)
+        rank_same_data['Rank'].append(rank_same)
+
+    rank_same_df = pd.DataFrame(rank_same_data)
+    st.dataframe(rank_same_df)
